6a0cd1e7769ed1a925123e058338ef7734d6247b
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2024-06-25 19:17:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const serveStatic = require("serve-static");
+
+const express = require("express");
+const app = express();
+
+//load config from env file
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+//middleware to parse json request body
+app.use(express.json());
+app.use(serveStatic("public"));
+
+//import routes for TODO API
+const todoRoutes = require("./routes/todos");
+
+//mount the todo API routes
+app.use("/api/v1", todoRoutes);
+
+//start server
+app.listen(PORT, () => {
+  console.log(`Server started successfully at ${PORT}`);
+});
+
+//connect to the database
+const dbConnect = require("./config/database");
+dbConnect();
+
+//default Route
+app.get("/", (req, res) => {
+  res.send(`<h1> I Love You Baby </h1>`);
+});
